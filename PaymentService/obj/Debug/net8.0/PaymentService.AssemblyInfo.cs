24fa2e91fbcef10888ce9032c66c6cc06e2525ee
chaitanyapatiballa <chaitanyapatiballa@gmail.com>
2025-06-03 18:13:51 +0530

    Update project structure and Docker configuration

--- PaymentService/obj/Debug/net8.0/PaymentService.AssemblyInfo.cs
+++ PaymentService/obj/Debug/net8.0/PaymentService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5637b67974d0e6e8e5b48aec92a0dc38912aad")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
