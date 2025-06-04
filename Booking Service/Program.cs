7961bf105a366125a68a5976deef66c58f729ece
chaitanyapatiballa <chaitanyapatiballa@gmail.com>
2025-06-04 12:11:20 +0530

    Add JWT authentication and related middleware

--- Booking Service/Program.cs
+++ Booking Service/Program.cs
@@ -1,7 +1,10 @@
-using Booking_Service.Data;
+﻿using Booking_Service.Data;
 using Booking_Service.Repositories;
 using Booking_Service.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +19,25 @@ builder.Services.AddDbContext<BookingDbContext>(options =>
 
 // Register Repositories and Services
 builder.Services.AddScoped<BookingRepository>();
-builder.Services.AddHttpClient(); // Needed for HttpClient injection
+builder.Services.AddHttpClient();
 builder.Services.AddScoped<BookingServices>();
+
+//  JWT Authentication
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Secret"]!))
+        };
+    });
 
 var app = builder.Build();
 
@@ -28,7 +48,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication(); 
 app.UseAuthorization();
+
 app.MapControllers();
-app.Run();
 app.Run();
